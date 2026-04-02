ec2b3bd79c0d0c2b7e0f903660b4b9121f5398ef
Nidhi <nidhimeswania2455@gmail.com>
2026-04-02 10:17:22 +0530

    Add files via upload

---
+++ Views/AppointmentView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace AppointmentSystem.Views
+{
+    public partial class AppointmentView : UserControl
+    {
+        public AppointmentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/BookAppointmentView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace AppointmentSystem.Views
+{
+    public partial class BookAppointmentView : UserControl
+    {
+        public BookAppointmentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
